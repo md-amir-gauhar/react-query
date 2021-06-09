4665218991a5808f6c974f981eefc7af4f5e23c5
md-amir-gauhar <gauharamir15@gmail.com>
2021-06-09 15:02:58 +0530

    imported useQuery hooks and implemented it with a button

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 import './App.css';
+import { useQuery } from 'react-query';
+import { useState } from 'react';
+
+function Button() {
+  const { data, error } = useQuery('hello-world', () => {
+    return new Promise(resolve => {
+      setTimeout(() => resolve(Math.random()), 2000)
+    })
+  })
+
+  return <button>{data}{error}</button>
+}
 
 function App() {
+  const [visible, setVisible] = useState(true);
   return (
     <div className="App">
-      
+      {visible && <Button />}
+      <button onClick={() => setVisible(visible => !visible)}>click</button>
+      <h1>Hello World</h1>
     </div>
   );
 }
